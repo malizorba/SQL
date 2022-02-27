c329f447f19146fb20a4cac61337526d424e26e1
malizorba <97551928+malizorba@users.noreply.github.com>
2022-02-27 18:01:56 +0300

    SQL has been added

---
+++ ODEV1.sql
@@ -0,0 +1,16 @@
+--SELECT title FROM film;
+
+--SELECT * FROM film;
+--SELECT first_name,last_name FROM actor;
+--SELECT title,description FROM film;
+--SELECT * FROM film
+--WHERE length > 60 AND length <70 ;
+--WHERE rental_rate =0.99 AND replacement_cost=12.99 OR replacement_cost=28.99;
+--SELECT * FROM customer
+--WHERE first_name = 'Mary';
+--SELECT * FROM film
+--WHERE length!=50 AND (rental_rate!=2.99 OR rental_rate !=4.99);
+
+
+
+
