f41dc107acabd6cb3122c4e4282ee4f3e4f9cba0
malizorba <97551928+malizorba@users.noreply.github.com>
2022-02-28 19:48:26 +0300

    Create ödev.3.sql
    hw 3 has been created

---
+++ ödev.3.sql
@@ -0,0 +1,7 @@
+--SELECT * FROM country
+--WHERE country LIKE 'A%a'
+--WHERE country LIKE '_____%n'
+
+SELECT * FROM film
+--WHERE title ILIKE '%t%t%t%t%'
+--WHERE title LIKE 'C%' AND length >90 AND rental_rate=2.99
