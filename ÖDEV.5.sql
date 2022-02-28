5b764b57b8168a575f927b955dc49fb30992b77b
malizorba <97551928+malizorba@users.noreply.github.com>
2022-02-28 21:15:06 +0300

    HW 5 has been added

---
+++ ÖDEV.5.sql
@@ -0,0 +1,14 @@
+--SELECT * FROM film
+--WHERE title ILIKE '%n'
+--ORDER BY length DESC
+--LIMIT 5;
+
+--WHERE title ILIKE '%n'
+--ORDER BY length DESC
+--OFFSET 5
+--LIMIT 5;
+
+SELECT * FROM customer
+WHERE store_id =1
+ORDER BY last_name ASC
+LIMIT 4;
