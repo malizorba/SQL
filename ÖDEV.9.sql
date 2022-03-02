e4071d5de45b3cecd1039a7eb37fe5359cd6fb5e
malizorba <97551928+malizorba@users.noreply.github.com>
2022-03-02 23:00:32 +0300

    HW 9 has been created

---
+++ ÖDEV.9.sql
@@ -0,0 +1,9 @@
+--SELECT city.city,country.country FROM city
+--INNER JOIN country ON city.country_id=country.country_id;
+
+--SELECT first_name,last_name FROM payment
+--INNER JOIN customer ON payment.customer_id=customer.customer_id;
+
+--SELECT first_name,last_name FROM rental
+--INNER JOIN customer ON rental.customer_id=customer.customer_id;
+
