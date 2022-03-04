07f4c76cb35292bfdc2730a34e8033e4eb99904a
malizorba <97551928+malizorba@users.noreply.github.com>
2022-03-04 22:14:01 +0300

    Create ÖDEV11.sql

---
+++ ÖDEV11.sql
@@ -0,0 +1,29 @@
+--(
+--SELECT first_name FROM actor
+	--ORDER BY first_name
+	--)
+--UNION 
+--(
+--SELECT first_name from actor
+	--ORDER BY first_name
+	--)
+	
+--(
+--SELECT first_name FROM actor
+	--ORDER BY first_name
+	--)
+--INTERSECT 
+--(
+--SELECT first_name from actor
+	--ORDER BY first_name
+	--)
+	
+--(
+--SELECT first_name FROM actor
+--)
+	--EXCEPT
+--(
+--SELECT first_name FROM customer
+	--)
+	
+4. soru için EXCEPT UNION VE INTERSECT SONUNA ALL EKLE.
